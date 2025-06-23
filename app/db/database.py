ca0c56886de1d15263aa06587241a18e6cc2981d
MartinWeigl125 <martinweigljun@gmail.com>
2025-06-23 09:21:43 +0200

    ich mach's anders

--- app/db/database.py
+++ app/db/database.py
@@ -3,7 +3,7 @@ from supabase import create_client, Client
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
